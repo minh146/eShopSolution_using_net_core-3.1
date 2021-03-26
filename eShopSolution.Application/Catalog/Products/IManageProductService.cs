29c4329bb7229b1164f9dde21880b1b75f679c86
minh1467 <duongquangminh0410@gmail.com>
2021-03-26 18:03:27 +0700

    Applications

---
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,31 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+
+{
+    public interface IManageProductService
+    {
+        Task<int> Create(ProductsCreateRequest request);
+
+        Task<int> Update(ProductUpdateRequest request);
+
+        Task<int> Delete(int productId);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        Task<List<ProductViewModel>> GetAll();
+        Task AddViewcount(int productId);
+
+        Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize);
+
+
+
+
+
+
+
+    }
---
+++ eShopSolution.Application/Catalog/Products/IPublicProductSevice.cs
@@ -0,0 +1,12 @@
+﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+   public interface IPublicProductSevice
+    {
+        public PagedViewModel<ProductViewModel> GetAllByCategoryId(int categoryId, int pageIndex, int pageSize);
+    }
+}
